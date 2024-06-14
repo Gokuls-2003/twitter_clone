6f31360cfbb34da8952104c207d6c22b0f3d0c3a
GOKUL SENTHIL <gokulsenthil899@gmail.com>
2024-06-14 14:07:04 +0530

    first commit

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppWriteConstants {
+  static const String databaseId = '666bff14002569eea06e';
+  static const String projectId = '666be8b0001bd89655a8';
+  static const String endPoint = 'http://localhost:80';
+}
