c6250ad81562292db7ead119d155f4c9457c3d25
GOKUL SENTHIL <gokulsenthil899@gmail.com>
2024-06-26 12:46:43 +0530

    Side Drawer UI & Navigation to Current User Profile

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:twitter_clone/constants/constants.dart';
+import 'package:twitter_clone/features/home/widgets/side_drawer.dart';
 import 'package:twitter_clone/features/tweet/views/create_tweet_view.dart';
 import 'package:twitter_clone/theme/theme.dart';
 
@@ -44,6 +45,7 @@ class _HomeViewState extends State<HomeView> {
         size: 28,
       ),
     ),
+     drawer: const SideDrawer(),
       bottomNavigationBar: CupertinoTabBar(
         currentIndex: _page,
         onTap: OnPageChange,
---
+++ lib/features/home/widgets/side_drawer.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter_clone/common/common.dart';
+import 'package:twitter_clone/features/auth/controller/auth_controller.dart';
+import 'package:twitter_clone/features/user_profile/view/user_profile_view.dart';
+import 'package:twitter_clone/theme/pallate.dart';
+
+class SideDrawer extends ConsumerWidget {
+  const SideDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentUser = ref.watch(currentUserDetailsProvider).value;
+   if(currentUser == null){
+    return const Loader();
+   }
+   
+    return SafeArea(
+      child: Drawer(
+        backgroundColor: Pallete.backgroundColor,
+        child: Column(
+          children: [
+            const SizedBox(height: 50,),
+            ListTile(
+              leading: const Icon(
+                Icons.person,
+                size: 30,
+                ),
+                title: Text("My Profile", style: TextStyle(
+                  fontSize: 22
+                ),),
+                onTap: (){ 
+                  Navigator.push(context, UserProfileView.route(currentUser));
+                },
+            ),
+               ListTile(
+              leading: const Icon(
+                Icons.payment,
+                size: 30,
+                ),
+                title: Text("Twitter Blue", style: TextStyle(
+                  fontSize: 22
+                ),),
+                onTap: (){ },
+            ),
+               ListTile(
+              leading: const Icon(
+                Icons.logout,
+                size: 30,
+                ),
+                title: Text("Log out", style: TextStyle(
+                  fontSize: 22
+                ),),
+                onTap: (){ },
+            )
+          ],
+        ),
+      ) );
+  }
+}
